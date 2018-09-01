cb8cf34f1afa4ea573649702e213ec8ca06cafe4
Karolina <karolinaa3004@gmail.com>
2018-09-01 10:33:50 +0200

    [#1.1]

--- App.js
+++ App.js
@@ -1,23 +1,50 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { Component } from 'react';
+import { View, StyleSheet, FlatList, Text } from 'react-native';
+import { Constants } from 'expo';
 
-export default class App extends React.Component {
+export default class App extends Component {
+  state = {
+    items: new Array(1).fill(0).map((a, i) => i).map(i => ({
+      title: `Notatka ${i}`,
+      key: i,
+      content: `Kontent notatki ${i}. Treść notatki jest taka jaka jest. Może być nawet trochę dłuższa.`,
+    })),
+  };
+
   render() {
     return (
       <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-        <Text>Changes you make will automatically reload.</Text>
-        <Text>Shake your phone to open the developer menu.</Text>
+        <FlatList data={this.state.items} renderItem={this.renderItem} />
       </View>
     );
   }
+
+  renderItem = ({ item }) => (
+    <View style={styles.item}>
+      <Text style={styles.title}>{item.title}</Text>
+      <Text style={styles.content}>{item.content}</Text>
+    </View>
+  );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+    padding: 0,
+    margin: 0,
+    paddingTop: Constants.statusBarHeight,
+    backgroundColor: '#ffffe6',
+  },
+  item: {
+    paddingHorizontal: 10,
+  },
+  title: {
+    fontWeight: 'bold',
+    marginVertical: 5,
+  },
+  content: {
+    marginBottom: 10,
   },
 });
